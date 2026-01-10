45bf5a0b111273cbbaecb6d8e6a129db6a751778
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2026-01-10 21:06:28 +0530

    Update LoopCard.jsx

--- frontend/src/components/LoopCard.jsx
+++ frontend/src/components/LoopCard.jsx
@@ -241,7 +241,7 @@ function LoopCard({loop}) {
 </div>
           {loop.author?._id === userData._id && (
   <div
-    className="absolute top-[20px] left-[20px] z-[200] cursor-pointer bg-black/50 p-[6px] rounded-full"
+    className="absolute top-[120px] left-[5px] z-[200] cursor-pointer bg-black/50 p-[6px] rounded-full"
     onClick={handleDeleteLoop}
   >
     <MdDelete className="w-[22px] h-[22px] text-red-500" />
