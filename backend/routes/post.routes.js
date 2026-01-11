39ab0473ce7038a1341e684a5db18580272ea37e
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2026-01-11 11:28:26 +0530

    Update post.routes.js

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
-import { comment, deletePost, getAllPosts, like, saved, uploadPost } from "../controllers/post.controllers.js"
+import { comment, deletePost, getAllPosts, like, saved, uploadPost,deleteComment } from "../controllers/post.controllers.js"
 
 
 const  postRouter= express.Router()
@@ -11,7 +11,11 @@ postRouter.get("/getAll", isAuth, getAllPosts)
 postRouter.get("/like/:postId", isAuth, like)
 postRouter.post("/comment/:postId", isAuth,comment)
 postRouter.get("/saved/:postId", isAuth,saved)
-
+postRouter.delete(
+  "/comment/:postId/:commentId",
+  isAuth,
+  deleteComment
+)
 postRouter.delete("/delete/:postId", isAuth, deletePost)
 
 
