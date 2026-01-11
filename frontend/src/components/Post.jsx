2ad68f6825be8eb1906e9df9b7d712d12f4ce2fd
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2026-01-11 11:47:49 +0530

    Update Post.jsx

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -173,35 +173,38 @@ useEffect(()=>{
 
             <div className='w-full max-h-[300px] overflow-auto'>
 
-{post.comments?.map((com) => (
+{post.comments?.map((com, index) => (
   <div
-    key={com._id}
-    className='w-full px-[20px] py-[20px] flex justify-between items-center gap-[20px] border-b-2 border-b-gray-200'
+    key={index}
+    className="w-full px-[20px] py-[20px] flex items-center justify-between gap-[20px] border-b-2 border-b-gray-200"
   >
-    <div className='flex items-center gap-[20px]'>
-      <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] border-2 border-black rounded-full cursor-pointer overflow-hidden'>
+    <div className="flex items-center gap-[15px]">
+      <div className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] border-2 border-black rounded-full overflow-hidden">
         <img
           src={com.author?.profileImage || dp}
           alt=""
-          className='w-full object-cover'
+          className="w-full h-full object-cover"
         />
       </div>
 
       <div>
-        <p className='font-semibold'>{com.author?.userName}</p>
-        <p>{com.message}</p>
+        <p className="font-semibold text-[14px]">
+          {com.author?.userName}
+        </p>
+        <p className="text-[13px]">{com.message}</p>
       </div>
     </div>
 
     {(userData._id === com.author?._id ||
       userData._id === post.author?._id) && (
       <MdDelete
-        className='w-[20px] h-[20px] text-red-600 cursor-pointer'
+        className="text-red-600 cursor-pointer"
         onClick={() => handleDeleteComment(com._id)}
       />
     )}
   </div>
 ))}
+
 
             </div>
       </div>}
