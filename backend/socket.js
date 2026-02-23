9adc25dabfcb928b3bcb41f4c54fa11ee7f2b5a4
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2026-02-23 16:06:15 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -16,7 +16,7 @@ const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
         // origin : process.env.FRONTEND_URL,
-        origin : "https://ai-app-roan-psi.vercel.app",,
+        origin : "https://ai-app-roan-psi.vercel.app",
         methods:["GET","POST"],
         credentials: true
     }
